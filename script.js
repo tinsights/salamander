16fc778d17e958577720c79ee1a3d68a99a51ea7
mister T <aj.tinaes@icloud.com>
2023-02-15 13:01:35 +0800

    2006 electoral boundaries display test done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("hello world")
+window.addEventListener('DOMContentLoaded', async () => {
+  const map = L.map('map').setView([1.3521, 103.8198], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  const test = await axios.get('data/electoral-boundary-2006/electoral-boundary-2006-geojson.geojson');
+  console.log(test.data);
+  const testLayer = L.geoJson(test.data).addTo(map);
+});
