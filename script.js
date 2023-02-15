e61e54eb87bd5f419062903f0128b7c09009483c
mister T <aj.tinaes@icloud.com>
2023-02-15 17:38:37 +0800

    label marker for postal codes

--- script.js
+++ script.js
@@ -8,19 +8,27 @@ window.addEventListener('DOMContentLoaded', async () => {
 
   const twoThousandSix = await axios.get('data/electoral-boundary-2006/electoral-boundary-2006-geojson.geojson');
   console.table(twoThousandSix.data);
-  const twoThousandSixLayer = L.geoJson(twoThousandSix.data).addTo(map);
+  const twoThousandSixLayer = L.geoJson(twoThousandSix.data, {
+    onEachFeature: (feature, layer) => { layer.bindPopup(feature.properties.Description); },
+  });
 
   const twoThousandEleven = await axios.get('data/electoral-boundary-2011/electoral-boundary-2011-geojson.geojson');
   console.table(twoThousandEleven.data);
-  const twoThousandElevenLayer = L.geoJson(twoThousandEleven.data).addTo(map);
+  const twoThousandElevenLayer = L.geoJson(twoThousandEleven.data, {
+    onEachFeature: (feature, layer) => { layer.bindPopup(feature.properties.Description); },
+  });
 
   const twentyFifteen = await axios.get('data/electoral-boundary-2015/electoral-boundary-2015-geojson.geojson');
   console.table(twentyFifteen.data);
-  const twentyFifteenLayer = L.geoJson(twentyFifteen.data).addTo(map);
+  const twentyFifteenLayer = L.geoJson(twentyFifteen.data, {
+    onEachFeature: (feature, layer) => { layer.bindPopup(feature.properties.Description); },
+  });
 
   const twentyTwenty = await axios.get('data/electoral-boundary_2020/doc.geojson');
   console.table(twentyTwenty.data);
-  const twentyTwentyLayer = L.geoJson(twentyTwenty.data).addTo(map);
+  const twentyTwentyLayer = L.geoJson(twentyTwenty.data, {
+    onEachFeature: (feature, layer) => { layer.bindPopup(feature.properties.Name); },
+  });
 
   const baseLayers = {
     2006: twoThousandSixLayer,
@@ -32,4 +40,13 @@ window.addEventListener('DOMContentLoaded', async () => {
   const overlays = {
   };
   L.control.layers(baseLayers, overlays).addTo(map);
+
+  const postalCodeBtn = document.getElementById('postalCodeBtn');
+  postalCodeBtn.addEventListener('click', async () => {
+    const address = await axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${postalCodeSelector.value}&returnGeom=Y&getAddrDetails=N`);
+    console.log(address.data.results[0]);
+    const coordinates = address.data.results[0];
+    const addressMarker = L.marker([coordinates.LATITUDE, coordinates.LONGITUDE]);
+    addressMarker.addTo(map);
+  });
 });
