1597cb6aaff75a8f1297a4571fa0bee6a92e532a
mister T <aj.tinaes@icloud.com>
2023-02-17 16:00:19 +0800

    refactored into functions

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 /* eslint-disable no-use-before-define */
 window.addEventListener('DOMContentLoaded', async () => {
   const map = initMap();
-  await addLayersToMap(map);
-  await addPostalSearchEvent(map);
+  addLayersToMap(map);
+  addPostalSearchEvent(map);
 });
 
 function initMap() {
@@ -17,57 +17,12 @@ function initMap() {
 }
 
 async function addLayersToMap(map) {
-  const twoThousandSix = await axios.get('data/electoral-boundary-2006/electoral-boundary-2006-geojson.geojson');
-  // console.table(twoThousandSix.data);
-  const twoThousandSixResults = await electionResults(2006);
-
-  twoThousandSixResults.forEach((result) => {
-    result.constituency = result.constituency.replace('-', ' - ');
-    console.log(result.constituency);
-  });
-  const twoThousandSixLayer = L.geoJson(twoThousandSix.data, {
-    onEachFeature: (feature, layer) => {
-      const constituencyDescription = htmlTableToJson(feature.properties.Description);
-      const constituencyName = constituencyDescription.ED_DESC.trim();
-      const constituencyResults = twoThousandSixResults.filter((result) => result.constituency.toUpperCase() === constituencyName);
-      layer.bindPopup(JSON.stringify(constituencyResults));
-    },
-  });
-
-  const twoThousandEleven = await axios.get('data/electoral-boundary-2011/electoral-boundary-2011-geojson.geojson');
-  // console.table(twoThousandEleven.data);
-  const twoThousandElevenResults = await electionResults(2011);
-  const twoThousandElevenLayer = L.geoJson(twoThousandEleven.data, {
-    onEachFeature: (feature, layer) => {
-      const constituencyDescription = htmlTableToJson(feature.properties.Description);
-      const constituencyName = constituencyDescription.ED_DESC.trim();
-      const constituencyResults = twoThousandElevenResults.filter((result) => result.constituency.toUpperCase() === constituencyName);
-      layer.bindPopup(JSON.stringify(constituencyResults));
-    },
-  });
-
-  const twentyFifteen = await axios.get('data/electoral-boundary-2015/electoral-boundary-2015-geojson.geojson');
-  const twentyFifteenResults = await electionResults(2015);
-  // console.table(twentyFifteen.data);
-  const twentyFifteenLayer = L.geoJson(twentyFifteen.data, {
-    onEachFeature: (feature, layer) => {
-      const constituencyDescription = htmlTableToJson(feature.properties.Description);
-      const constituencyName = constituencyDescription.ED_DESC.trim();
-      const constituencyResults = twentyFifteenResults.filter((result) => result.constituency.toUpperCase() === constituencyName);
-      layer.bindPopup(JSON.stringify(constituencyResults));
-    },
-  });
-
-  const twentyTwenty = await axios.get('data/electoral-boundary-2020/electoral-boundary-2020-geojson.geojson');
-  // console.table(twentyTwenty.data);
-  const twentyTwentyResults = await electionResults(2020);
-  const twentyTwentyLayer = L.geoJson(twentyTwenty.data, {
-    onEachFeature: (feature, layer) => {
-      const constituencyName = feature.properties.Name;
-      const constituencyResults = twentyTwentyResults.filter((result) => result.constituency.toUpperCase() === constituencyName);
-      layer.bindPopup(JSON.stringify(constituencyResults));
-    },
-  });
+  const [
+    twoThousandSixLayer,
+    twoThousandElevenLayer,
+    twentyFifteenLayer,
+    twentyTwentyLayer,
+  ] = await Promise.all([createLayer(2006), createLayer(2011), createLayer(2015), createLayer(2020)]);
 
   twentyTwentyLayer.addTo(map);
 
@@ -85,7 +40,7 @@ async function addPostalSearchEvent(map) {
   const postalCodeBtn = document.getElementById('postalCodeBtn');
   postalCodeBtn.addEventListener('click', async () => {
     const address = await axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${postalCodeSelector.value}&returnGeom=Y&getAddrDetails=N`);
-    console.log(address.data.results[0]);
+    // console.log(address.data.results[0]);
     const coordinates = address.data.results[0];
     const addressMarker = L.marker([coordinates.LATITUDE, coordinates.LONGITUDE]);
     addressMarker.bindPopup(coordinates.SEARCHVAL);
@@ -93,18 +48,50 @@ async function addPostalSearchEvent(map) {
   });
 }
 
+// function htmlTableToJson(htmlTable) {
+//   const e = document.createElement('div');
+//   e.innerHTML = htmlTable;
+//   const ths = Array.from(e.querySelectorAll('th'));
+//   const tds = Array.from(e.querySelectorAll('td'));
+//   const constituencyDescription = Object.fromEntries(tds.map((td, idx) => [ths[idx + 1].innerText, td.innerText]));
+//   return constituencyDescription;
+// }
+
+async function createLayer(year) {
+  const [yearResults, yearBoundaries] = await getYearLayerData(year);
+  const yearLayer = L.geoJson(yearBoundaries, {
+    onEachFeature: (feature, layer) => {
+      // console.log(feature.properties);
+      const constituencyName = feature.properties.ED_DESC;
+      const constituencyResults = yearResults.filter((result) => result.constituency.toUpperCase() === constituencyName);
+      layer.bindPopup(JSON.stringify(constituencyResults));
+    },
+  });
+  return yearLayer;
+}
+
+async function getYearLayerData(year) {
+  const yearResultsReq = electionResults(year);
+  const yearBoundariesReq = electionBoundaries(year);
+  return Promise.all([yearResultsReq, yearBoundariesReq]);
+}
+
 async function electionResults(year) {
   const yearResultsResponse = await axios.get(`https://data.gov.sg/api/action/datastore_search?resource_id=4706f2cb-a909-4cc0-bd3d-f366c34cf6af&q=${year}`);
   const yearResults = yearResultsResponse.data.result.records;
-  // console.log(yearResults);
+  if (year === 2006) {
+    console.log(yearResults);
+  }
   return yearResults;
 }
 
-function htmlTableToJson(htmlTable) {
-  const e = document.createElement('div');
-  e.innerHTML = htmlTable;
-  const ths = Array.from(e.querySelectorAll('th'));
-  const tds = Array.from(e.querySelectorAll('td'));
-  const constituencyDescription = Object.fromEntries(tds.map((td, idx) => [ths[idx + 1].innerText, td.innerText]));
-  return constituencyDescription;
+async function electionBoundaries(year) {
+  const yearBoundariesResponse = await axios.get(`data/electoral-boundary-${year}/electoral-boundary-${year}-kml.geojson`);
+  const yearBoundaries = yearBoundariesResponse.data;
+  if (year === 2006) {
+    yearBoundaries.features.forEach((feature, idx) => {
+      feature.properties.ED_DESC = feature.properties.ED_DESC.trim().replace(' - ', '-');
+    });
+  }
+  return yearBoundaries;
 }
