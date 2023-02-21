f6be5ec908901fe3e7689ab01180166588a2214e
mister T <aj.tinaes@icloud.com>
2023-02-21 18:45:25 +0800

    mvc?

--- script.js
+++ script.js
@@ -1,71 +1,27 @@
-/* eslint-disable max-len */
-/* eslint-disable no-use-before-define */
-const globalLayers = [];
 window.addEventListener('DOMContentLoaded', async () => {
-  const map = initMap();
-  addLayersToMap(map);
-  addPostalSearchEvent(map);
+  const view = initMap();
+  const model = await generateModel([2006, 2011, 2015, 2020]);
+  addLayersToMap(model, view);
+  addPostalSearchEvent(model, view);
 });
 
-function initMap() {
-  const map = L.map('map').setView([1.3521, 103.8198], 13);
-
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
-  return map;
+/**
+ * MODEL
+ * takes in an array of election years as input,
+ * creates an object with year as keys, Leaflet layer element as value.
+ *
+ */
+async function generateModel(years) {
+  const layerArr = await Promise.all(years.map((year) => createLayer(year)));
+  return Object.fromEntries(years.map((year, idx) => [year, layerArr[idx]]));
 }
 
-async function addLayersToMap(map) {
-  const [
-    twoThousandSixLayer,
-    twoThousandElevenLayer,
-    twentyFifteenLayer,
-    twentyTwentyLayer,
-  ] = await Promise.all([createLayer(2006), createLayer(2011), createLayer(2015), createLayer(2020)]);
-  twentyTwentyLayer.addTo(map);
-
-  const baseLayers = {
-    2006: twoThousandSixLayer,
-    2011: twoThousandElevenLayer,
-    2015: twentyFifteenLayer,
-    2020: twentyTwentyLayer,
-  };
-  L.control.layers(baseLayers).addTo(map);
-  return null;
-}
-
-async function addPostalSearchEvent(map) {
-  const postalCodeBtn = document.getElementById('postalCodeBtn');
-  postalCodeBtn.addEventListener('click', async () => {
-    const address = await axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${postalCodeSelector.value}&returnGeom=Y&getAddrDetails=N`);
-    // console.log(address.data.results[0]);
-    const coordinates = address.data.results[0];
-    const addressMarker = L.marker([coordinates.LATITUDE, coordinates.LONGITUDE]);
-    addressMarker.bindPopup(coordinates.SEARCHVAL);
-    findConstituencies(addressMarker);
-    addressMarker.addTo(map);
-  });
-}
-
-function findConstituencies(addressMarker) {
-  console.log(addressMarker);
-  console.log(globalLayers);
-  const polygon = L.polygon([
-    [1, 100, 0],
-    [1, 105, 0],
-    [1.5, 105, 0],
-    [1.5, 100, 0],
-  ]);
-  console.log(polygon);
-  console.log(polygon.contains(addressMarker.getLatLng()));
-  globalLayers.forEach((yearLayer) => yearLayer.eachLayer((sublayer) => {
-    if (sublayer.contains(addressMarker.getLatLng())) {
-      console.log(sublayer.feature.properties);
-    }
-  }));
-}
+/**
+ * creates a Leaflet Layer object from GeoJSON data
+ * layer is an array of polygons that define constituencies
+ * relevant election data is contained in properties of each constituency
+ * @returns {Object}
+ */
 async function createLayer(year) {
   const layerData = await getYearLayerData(year);
   const yearLayer = L.geoJson(layerData, {
@@ -89,25 +45,24 @@ async function createLayer(year) {
           return {
             color: 'blue', // `rgb(${(1 - winner.vote_percentage) * 255}, 0, ${winner.vote_percentage * 255})`,
             fillColor: `rgb(${(1 - winner.vote_percentage) * 255}, 0, ${winner.vote_percentage * 255})`,
-            fillOpacity: +winner.vote_percentage * 0.7,
+            fillOpacity: Math.min(winner.vote_percentage, 0.7),
           };
         }
         default: return {
           color: 'red', // `rgb(${(1 - winner.vote_percentage) * 255}, 0, ${winner.vote_percentage * 255})`,
           fillColor: `rgb(${winner.vote_percentage * 255}, 0, ${(1 - winner.vote_percentage) * 255})`,
-          fillOpacity: +winner.vote_percentage * 0.7,
+          fillOpacity: Math.min(winner.vote_percentage, 0.7),
 
         };
       }
     },
   });
-  globalLayers.push(yearLayer);
   return yearLayer;
 }
 
 async function getYearLayerData(year) {
-  const yearResultsReq = electionResults(year);
-  const yearBoundariesReq = electionBoundaries(year);
+  const yearResultsReq = getElectionResults(year);
+  const yearBoundariesReq = getElectionBoundaries(year);
 
   return Promise.all([yearResultsReq, yearBoundariesReq])
     .then(([yearResultsResponse, yearBoundariesResponse]) => {
@@ -119,22 +74,59 @@ async function getYearLayerData(year) {
     });
 }
 
-/**
- * requests General election results data from data.gov.sg API
- * @returns {Object} in JSON
- */
-async function electionResults(year) {
+async function getElectionResults(year) {
   const yearResultsResponse = await axios.get(`https://data.gov.sg/api/action/datastore_search?resource_id=4706f2cb-a909-4cc0-bd3d-f366c34cf6af&q=${year}`);
   const yearResults = yearResultsResponse.data.result.records;
   return yearResults;
 }
 
-/**
- * reads electoral boundary data for a specific year
- * @returns {Object} in geoJSON format
- */
-async function electionBoundaries(year) {
+async function getElectionBoundaries(year) {
   const yearBoundariesResponse = await axios.get(`data/electoral-boundary-${year}/electoral-boundary-${year}-kml.geojson`);
   const yearBoundaries = yearBoundariesResponse.data;
   return yearBoundaries;
+}
+
+function initMap() {
+  const map = L.map('map').setView([1.3521, 103.8198], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  return map;
+}
+
+function addLayersToMap(model, view) {
+  const baseLayers = model;
+  const years = Object.keys(model);
+  model[years[years.length - 1]].addTo(view);
+  L.control.layers(baseLayers).addTo(view);
+  return null;
+}
+
+async function addPostalSearchEvent(model, view) {
+  const postalCodeBtn = document.getElementById('postalCodeBtn');
+  postalCodeBtn.addEventListener('click', async () => {
+    const address = await axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${postalCodeSelector.value}&returnGeom=Y&getAddrDetails=N`);
+    const coordinates = address.data.results[0];
+    const point = {
+      lat: coordinates.LATITUDE,
+      lng: coordinates.LONGITUDE,
+    };
+    const addressMarker = L.marker([point.lat, point.lng]);
+    const layers = Object.values(model);
+    const history = getHistory(layers, point);
+    addressMarker.bindPopup(JSON.stringify(history));
+    addressMarker.addTo(view);
+  });
+}
+
+function getHistory(layers, point) {
+  const constituencyHistory = [];
+  Object.values(layers).forEach((yearLayer) => yearLayer.eachLayer((sublayer) => {
+    if (sublayer.contains(point)) {
+      constituencyHistory.push(sublayer.feature.properties);
+    }
+  }));
+  console.log(constituencyHistory);
+  return constituencyHistory;
 }
