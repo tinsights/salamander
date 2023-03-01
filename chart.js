1a06d1b93700983f018c57a80f4d2b7fb3c7cbfc
mister T <aj.tinaes@icloud.com>
2023-03-01 15:26:33 +0800

    test

--- chart.js
+++ chart.js
@@ -6,17 +6,17 @@ function createChart(model) {
   const parties = {};
   labels.forEach((year, idx) => {
     Object.entries(model[year].RESULTS).forEach(([party, votes]) => {
-      party = party === "Independent" ? "INDP" : party;
+      party = party === 'Independent' ? 'INDP' : party;
       if (!parties[party]) {
         parties[party] = {
           label: party,
           data: Array(4).fill(0),
-          stack: "Stack 0",
+          stack: 'Stack 0',
         };
       }
       parties[party].data[idx] = votes.totalVotes;
-      if (party === "PAP") {
-        parties[party].stack = "Stack 1";
+      if (party === 'PAP') {
+        parties[party].stack = 'Stack 1';
       }
     });
   });
@@ -29,13 +29,13 @@ function createChart(model) {
   };
 
   const config = {
-    type: "bar",
+    type: 'bar',
     data,
     options: {
       plugins: {
         title: {
           display: true,
-          text: "Election Results 2006-2020",
+          text: 'Election Results 2006-2020',
         },
         legend: {
           display: false,
@@ -60,7 +60,74 @@ function createChart(model) {
     },
   };
 
-  const ctx = document.getElementById("myChart");
+  const ctx = document.getElementById('myChart');
 
   const chart = new Chart(ctx, config);
+
+  const canvas = document.getElementById('halfDonut');
+  const dashboardChart = new Chart(canvas, {
+    type: 'doughnut',
+    data: {
+      labels: ['Red'],
+      datasets: [{
+        label: '# of Votes',
+        data: [33],
+        backgroundColor: [
+          'rgba(231, 76, 60, 1)',
+        ],
+        borderColor: [
+          'rgba(255, 255, 255 ,1)',
+        ],
+        borderWidth: 5,
+      }],
+
+    },
+    options: {
+      rotation: 0,
+      circumference: (0.44 * 360),
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+      cutoutPercentage: 100,
+    },
+  });
+}
+
+function createDonut(vote_share, vote_percentage, party) {
+  console.log(vote_share);
+  const backgroundColor = ['#FF0000'];
+  let rotation = 0;
+  if (party === 'PAP') {
+    backgroundColor[0] = '#0000FF';
+    rotation = (1 - vote_percentage) * 360;
+  }
+  const container = document.createElement('div');
+  const donut = document.createElement('canvas');
+  const voteMeter = new Chart(donut, {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+        label: '# of Votes',
+        data: [Number(vote_share)],
+        backgroundColor,
+        borderWidth: 5,
+      }],
+
+    },
+    options: {
+      rotation,
+      circumference: (Number(vote_percentage) * 360),
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+  });
+  container.appendChild(donut);
+  return container;
 }
--- view.js
+++ view.js
@@ -2,8 +2,8 @@ function initView() {
   const initialHeight = window.innerWidth > 768 ? 11 : 10;
   console.log(window.innerWidth);
   console.log(initialHeight);
-  const map = L.map("map").setView([1.3521, 103.8198], initialHeight);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  const map = L.map('map').setView([1.3521, 103.8198], initialHeight);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
@@ -17,7 +17,7 @@ function initView() {
   const view = {
     map,
     layers: {},
-    currentStyle: "defaultStyle",
+    currentStyle: 'defaultStyle',
   };
   return view;
 }
@@ -31,7 +31,7 @@ function addLayersToMap(model, view) {
       timelineItems: years,
       extraChangeMapParams: { model, view },
       changeMap: timelineFunction,
-      position: "bottomleft",
+      position: 'bottomleft',
     })
     .addTo(view.map);
   return null;
@@ -44,7 +44,7 @@ function timelineFunction({ label, model, view }) {
 
   setView(constituencies, view.currentStyle);
 }
-function setView(constituencies, option = "defaultStyle") {
+function setView(constituencies, option = 'defaultStyle') {
   Object.values(constituencies).forEach((constituency) => {
     constituency.feature.setStyle(constituency.style[option]);
   });
@@ -55,7 +55,7 @@ function createLayer(model, view, year) {
     const geo = L.geoJSON(constituencyData.boundaries, {
       onEachFeature: (feature, layer) => {
         layer.bindPopup(createPopup(constituencyData), {
-          maxWidth: "fit-content",
+          maxWidth: 'fit-content',
         });
         console.log(window.innerWidth);
         if (window.innerWidth > 768) {
@@ -63,7 +63,7 @@ function createLayer(model, view, year) {
             sticky: true,
           });
         }
-        layer.on("click", () => layer.closeToolTip());
+        // layer.on('click', () => layer.closeToolTip());
       },
     });
     constituencyData.feature = geo;
@@ -75,87 +75,89 @@ function createLayer(model, view, year) {
 
 function createPopup(constituency) {
   const { results } = constituency;
-  const popup = document.createElement("div");
-  popup.classList.add("container");
-  const resultsDiv = document.createElement("div");
-  resultsDiv.classList.add("row", "gx-3", "flex-nowrap");
-  const constInfo = document.createElement("div");
-  constInfo.classList.add("container", "text-center");
+
+  const popup = document.createElement('div');
+  popup.classList.add('container');
+  const resultsDiv = document.createElement('div');
+  resultsDiv.classList.add('row', 'gx-3', 'flex-nowrap');
+  const constInfo = document.createElement('div');
+  constInfo.classList.add('container', 'text-center');
   constInfo.innerHTML = `<h5 class="h5">${results[0].constituency} ${results[0].constituency_type}</h5>`;
   popup.appendChild(constInfo);
   results.forEach((result) => {
-    const partyCol = document.createElement("div");
-    partyCol.classList.add("col", "flip-card");
+    const partyCol = document.createElement('div');
+    partyCol.classList.add('col', 'flip-card');
 
-    const flipCardInner = document.createElement("div");
-    flipCardInner.classList.add("flip-card-inner");
+    const flipCardInner = document.createElement('div');
+    flipCardInner.classList.add('flip-card-inner');
 
-    const partyCard = document.createElement("div");
+    const partyCard = document.createElement('div');
     partyCard.classList.add(
-      "flip-card-front",
-      "py-1",
-      "px-3",
-      "text-center",
-      "d-flex",
-      "flex-column",
-      "justify-content-center"
+      'flip-card-front',
+      'py-1',
+      'px-3',
+      'text-center',
+      'd-flex',
+      'flex-column',
+      'justify-content-center',
     );
 
-    const partyImg = document.createElement("img");
+    const partyImg = document.createElement('img');
     partyImg.src = partyImage(result.party);
-    partyImg.classList.add("logo");
+    partyImg.classList.add('logo');
 
-    const cardBody = document.createElement("p");
-    cardBody.classList.add("fw-bold");
-    const partyTitle = document.createElement("h6");
+    const cardBody = document.createElement('p');
+    cardBody.classList.add('fw-bold');
+    const partyTitle = document.createElement('h6');
     partyTitle.innerText = result.party;
-    partyTitle.classList.add("text-center", "h6");
-    cardBody.classList.add("px-0");
+    partyTitle.classList.add('text-center', 'h6');
+    cardBody.classList.add('px-0');
     const voteShare = Number(result.vote_percentage);
 
-    cardBody.innerHTML = `${(voteShare * 100).toFixed(2)}%`;
+    // cardBody.innerHTML = `${(voteShare * 100).toFixed(2)}%`;
+    cardBody.appendChild(createDonut(result.vote_count, result.vote_percentage, result.party));
 
-    const candidatesCard = document.createElement("div");
-    candidatesCard.classList.add("flip-card-back", "d-flex", "flex-column", "justify-content-center");
-    const candidatesList = document.createElement("ul");
-    candidatesList.classList.add("list-group", "text-center");
-    result.candidates.split(" | ").forEach((candidate) => {
-      const listItem = document.createElement("li");
-      listItem.classList.add("list-group-item", "p-1");
+    const candidatesCard = document.createElement('div');
+    candidatesCard.classList.add('flip-card-back', 'd-flex', 'flex-column', 'justify-content-center');
+    const candidatesList = document.createElement('ul');
+    candidatesList.classList.add('list-group', 'text-center');
+    result.candidates.split(' | ').forEach((candidate) => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('list-group-item', 'p-1');
       listItem.innerHTML = `${candidate}`;
       candidatesList.appendChild(listItem);
     });
 
     if (voteShare > 0.5) {
-      partyCard.classList.add("border", "border-success");
-      candidatesCard.classList.add("border", "border-success");
-      cardBody.classList.add("text-success");
+      partyCard.classList.add('border', 'border-success');
+      candidatesCard.classList.add('border', 'border-success');
+      cardBody.classList.add('text-success');
     } else {
-      partyCard.classList.add("border", "border-danger");
-      candidatesCard.classList.add("border", "border-danger");
-      cardBody.classList.add("text-danger");
+      partyCard.classList.add('border', 'border-danger');
+      candidatesCard.classList.add('border', 'border-danger');
+      cardBody.classList.add('text-danger');
     }
     candidatesCard.appendChild(candidatesList);
 
-    partyCard.addEventListener("click", () => {
-      partyCard.classList.add("flip-backwards");
-      candidatesCard.classList.add("flip-forwards");
+    partyCard.addEventListener('click', () => {
+      partyCard.classList.add('flip-backwards');
+      candidatesCard.classList.add('flip-forwards');
       setTimeout(() => {
-        partyCard.classList.add("flip-card-back");
-        partyCard.classList.remove("flip-card-front", "flip-backwards");
-        candidatesCard.classList.add("flip-card-front");
-        candidatesCard.classList.remove("flip-card-back", "flip-forwards");
+        partyCard.classList.add('flip-card-back');
+        partyCard.classList.remove('flip-card-front', 'flip-backwards');
+        candidatesCard.classList.add('flip-card-front');
+        candidatesCard.classList.remove('flip-card-back', 'flip-forwards');
       }, 1000);
     });
 
-    candidatesCard.addEventListener("click", () => {
-      partyCard.classList.add("flip-forwards");
-      candidatesCard.classList.add("flip-backwards");
+    candidatesCard.addEventListener('click', () => {
+      partyCard.classList.add('flip-forwards');
+      candidatesCard.classList.add('flip-backwards');
       setTimeout(() => {
-        candidatesCard.classList.add("flip-card-back");
-        candidatesCard.classList.remove("flip-card-front", "flip-backwards");
-        partyCard.classList.add("flip-card-front");
-        partyCard.classList.remove("flip-card-back", "flip-forwards");
+        candidatesCard.classList.add('flip-card-back');
+        candidatesCard.classList.remove('flip-card-front', 'flip-backwards');
+        partyCard.classList.add('flip-card-front');
+        partyCard.classList.remove('flip-card-back', 'flip-forwards');
       }, 1000);
     });
 
@@ -173,12 +175,12 @@ function createPopup(constituency) {
 
 function partyImage(party) {
   switch (party) {
-    case "INDP":
-    case "Independent":
-      return "assets/INDP_logo.png";
-    case "SUP":
-    case "PSP":
-    case "DPP":
+    case 'INDP':
+    case 'Independent':
+      return 'assets/INDP_logo.png';
+    case 'SUP':
+    case 'PSP':
+    case 'DPP':
       return `assets/${party}_logo.png`;
     default:
       return `assets/${party}_logo.svg`;
