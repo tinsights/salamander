438edae024ebaadc7fe9c1cc48e41168a7e86d6d
mister T <aj.tinaes@icloud.com>
2023-06-14 11:16:43 +0800

    changed search button icon

--- scripts/script.js
+++ scripts/script.js
@@ -13,46 +13,46 @@ window.addEventListener("DOMContentLoaded", async () => {
     addToggleButton(model, view);
     clearMarkersButton(view);
     darkmodeWatcher(view);
-    bsTour();
+    // bsTour();
   });
 });
 
-function bsTour() {
-  const tour = new Shepherd.Tour({
-    defaultStepOptions: {
-      cancelIcon: {
-        enabled: true,
-      },
-      classes: "class-1 class-2",
-      scrollTo: { behavior: "smooth", block: "center" },
-    },
-  });
+// function bsTour() {
+//   const tour = new Shepherd.Tour({
+//     defaultStepOptions: {
+//       cancelIcon: {
+//         enabled: true,
+//       },
+//       classes: "class-1 class-2",
+//       scrollTo: { behavior: "smooth", block: "center" },
+//     },
+//   });
 
-  tour.addStep({
-    title: "Creating a Shepherd Tour",
-    text: `Creating a Shepherd tour is easy. too!\
-  Just create a \`Tour\` instance, and add as many steps as you want.`,
-    attachTo: {
-      element: "#map",
-      on: "bottom",
-    },
-    buttons: [
-      {
-        action() {
-          return this.back();
-        },
-        classes: "shepherd-button-secondary",
-        text: "Back",
-      },
-      {
-        action() {
-          return this.next();
-        },
-        text: "Next",
-      },
-    ],
-    id: "creating",
-  });
+//   tour.addStep({
+//     title: "Creating a Shepherd Tour",
+//     text: `Creating a Shepherd tour is easy. too!\
+//   Just create a \`Tour\` instance, and add as many steps as you want.`,
+//     attachTo: {
+//       element: "#map",
+//       on: "bottom",
+//     },
+//     buttons: [
+//       {
+//         action() {
+//           return this.back();
+//         },
+//         classes: "shepherd-button-secondary",
+//         text: "Back",
+//       },
+//       {
+//         action() {
+//           return this.next();
+//         },
+//         text: "Next",
+//       },
+//     ],
+//     id: "creating",
+//   });
 
-  tour.start();
-}
+//   tour.start();
+// }
